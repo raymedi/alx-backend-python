050170986a2c884c73ed3e38c00e20812d9064c3
Ebimary <igwemary220@gmail.com>
2023-10-05 08:39:04 -0700

    backend

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,9 +1,17 @@
-#!/usr/bin/env python3
-'''Task 5's module.
-'''
 from typing import List
 
-
 def sum_list(input_list: List[float]) -> float:
-    '''Computes the sum of a list of floating-point numbers.
-    '''
+    """
+    Calculate the sum of a list of floats.
+    
+    Parameters:
+    input_list (List[float]): A list of floats.
+    
+    Returns:
+    float: The sum of the input_list elements.
+    """
+    return sum(input_list)
+True
+{'input_list': typing.List[float], 'return': <class 'float'>}
+sum_list(floats) returns 6.47 which is a <class 'float'>
+
