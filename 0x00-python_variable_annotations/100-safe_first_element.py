6f12817baf9ffac7eed5404dc979bad9abd82f60
Ebimary <igwemary220@gmail.com>
2023-10-05 08:13:22 -0700

    backend python

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''Task 10's module.
+'''
+from typing import Any, Sequence, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    '''Retrieves the first element of a sequence if it exists.
+    '''
---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''Task 11's module.
+'''
+from typing import Any, Mapping, Union, TypeVar
+
+
+T = TypeVar('T')
+Res = Union[Any, T]
+Def = Union[T, None]
